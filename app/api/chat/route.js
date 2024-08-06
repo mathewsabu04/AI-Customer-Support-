84b64f9c880d9d932161211c00f17cbef73dd47f
Mathew Sabu <internshipmathewsabu04@gmail.com>
2024-08-06 15:12:36 -0400

    Ended at 5:08 in the video and set up the POST function

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
 export async function POST(req) {
   const openai = new OpenAI();
   const data = await req.json();
-
   console.log(data);
   return NextResponse.json({ message: "Hello from the server" });
 }
